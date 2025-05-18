f1d29ee09806958db03e3da3f8f971f624fa90e0
Zikeji <me@zikeji.com>
2025-05-18 14:47:59 -0400

    feat: add `-v` or `--version` flag to output binary version

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,19 @@ mod monitor;
 
 use std::{thread, time::Duration};
 use std::collections::HashSet;
+use std::env;
 use env_logger::Env;
 
 #[cfg(not(target_os = "linux"))]
 compile_error!("This applications only works on Linux.");
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 && (args[1] == "-v" || args[1] == "--version") {
+        println!("linux-steam-icon-watcher v{}", env!("CARGO_PKG_VERSION"));
+        return;
+    }
+    
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let mut active_games: HashSet<(String, String)> = HashSet::new();
